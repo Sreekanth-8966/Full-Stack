e6f6c32b7f0dfd2a9f8a94409187c56af3027de3
Sreekanth-8966 <sreekanthsree12340@gmail.com>
2025-09-13 15:57:30 +0530

    commit

---
+++ Frontend/To Do LIst/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("#addTask").click(function () {
+        let task = $("#myInput").val().trim();
+
+        
+        if (task === "") {
+            alert("Please enter a task before adding!");
+            return;
+        }
+
+        $("#list").append(
+            `<li>
+                <span class="task-text">${task}</span>
+                <div class="button-group">
+                    <button class="green-btn">✅</button>
+                    <button class="red-btn">❌</button>
+                </div>
+            </li>`
+        );
+
+        $("#myInput").val("");
+        $("#output").text("");
+    });
+    $("#removeTask").click(function () {
+        $("#list li:last").remove();
+    });
+    $("#list").on("click", ".green-btn", function () {
+        const li = $(this).closest("li");
+        li.css("background-color", "#b2f2bb"); // light green
+    });
+    $("#list").on("click", ".red-btn", function () {
+        $(this).closest("li").remove();
+    });
+});
